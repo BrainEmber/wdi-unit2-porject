7d31c6e76e5fa60b0b111fe52a94ee65a0d620d0
Grant Dahlhauser <BrainEmber@Gmail.com>
2017-08-07 08:04:33 -0700

    updating package.json and updating code for heroku

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-Whatever bitch
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+
+const mongoUri = process.emv.MONGODB_URI || 'mongodb://localhost:27017/sc2'
+mongoose.connect(mongoUri);
+
+mongoose.connection.once('open', ()=>{
+	console.log('connected to mongo');
+});
+
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res)=>{
+  res.send('working')
+})
+
+
+
+app.listen(port, ()=>{
+  console.log('listening');
+})
