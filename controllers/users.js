091bb50e0e81813369210ffd189e57c0ec6dc836
Grant Dahlhauser <BrainEmber@Gmail.com>
2017-08-09 15:05:37 -0700

    edit and delete working

--- controllers/users.js
+++ controllers/users.js
@@ -19,8 +19,35 @@ router.post('/', (req, res)=>{
 
 router.get('/new', (req, res)=>{
   res.render('users/new.ejs')
-})
+});
 
+router.get('/:id', (req, res)=>{
+  User.findById(req.params.id, (err, foundUser)=>{
+    res.render('users/show.ejs', {
+      user: foundUser
+    });
+  });
+});
+
+router.delete('/:id', (req, res)=>{
+  User.findByIdAndRemove(req.params.id, (err, foundUser)=>{
+    res.redirect('/users')
+  });
+});
+
+router.get('/:id/edit', (req, res)=>{
+  User.findById(req.params.id, (err, foundUser)=>{
+    res.render('users/edit.ejs', {
+      user: foundUser
+    });
+  });
+});
+
+router.put('/:id', (req, res)=>{
+  User.findByIdAndUpdate(req.params.id, req.body, ()=>{
+    res.redirect('/users');
+  });
+});
 
 
 
